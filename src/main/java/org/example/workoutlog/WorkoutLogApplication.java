e7ed3ee1df62feb29b5220899eef6ace76741f33
Kanta Matsueda <kantamatsueda1@gmail.com>
2026-09-09 14:15:50 +0900

    Initial commit

---
+++ src/main/java/org/example/workoutlog/WorkoutLogApplication.java
@@ -0,0 +1,13 @@
+package org.example.workoutlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/workoutlog/WorkoutLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workoutlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
